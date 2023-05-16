587133eeb938be4737500b73349e7a587de9d9fc
Radek Włodarski <wlodarski.radek@gmail.com>
2023-05-16 19:13:08 +0200

    added store

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-import { App } from './core/App/App';
-import { ThemeProvider } from 'styled-components';
-import { theme } from '../src/core/App/theme'
-import { GlobalStyle } from './core/App/GlobalStyle';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./core/store";
+import { ThemeProvider } from "styled-components";
+import { theme } from "../src/core/App/theme";
+import { GlobalStyle } from "./core/App/GlobalStyle";
+import { App } from "./core/App/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
